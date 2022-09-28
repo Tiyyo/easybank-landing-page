c56afefc4a86ec023bf6d512dfc36782e9426f11
Tiyyo <111092370+Tiyyo@users.noreply.github.com>
2022-09-28 23:13:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const hamburgerBtn = document.querySelector(".hamburger--collapse");
 const navLinks = document.querySelector(".navbar__links");
-console.log(hamburgerBtn);
 
 hamburgerBtn.addEventListener("click", (e) => {
-  console.log(e);
   hamburgerBtn.classList.toggle("isactive");
   navLinks.classList.toggle("isactive");
 });
